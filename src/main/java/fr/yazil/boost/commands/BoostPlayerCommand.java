758f80315ca96c62e157db2d9a01118d8daa51f6
Yazil <yazil@gmx.fr>
2022-07-06 14:30:48 +0200

    Added /boost player subcommand

--- src/main/java/fr/yazil/boost/commands/BoostPlayerCommand.java
+++ src/main/java/fr/yazil/boost/commands/BoostPlayerCommand.java
@@ -1,5 +1,8 @@
 package fr.yazil.boost.commands;
 
+import fr.yazil.boost.boosts.BoostsManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -12,6 +15,40 @@ public class BoostPlayerCommand extends SubCommand{
 
     @Override
     public void onCommand(CommandSender commandSender, Command command, String[] args) {
+        if(args.length != 4){
+            commandSender.sendMessage(ChatColor.RED + "Invalid usage ! Correct usage: " + getUsage());
+            return;
+        }
 
+        Player player = Bukkit.getPlayer(args[1]);
+        if(player == null) {
+            commandSender.sendMessage(ChatColor.RED + "Player is whether not online or invalid.");
+            return;
+        }
+
+        float power;
+        try {
+            power = Float.parseFloat(args[2]);
+        } catch(NumberFormatException e){
+            commandSender.sendMessage(ChatColor.RED + "Power needs to be a valid number ! Ex: 2.5");
+            return;
+        }
+
+        int time;
+        try {
+            time = Integer.parseInt(args[3]);
+        } catch(NumberFormatException e) {
+            commandSender.sendMessage(ChatColor.RED + "Time needs to be a valid number ! Ex: 25");
+            return;
+        }
+
+        BoostsManager boostsManager = BoostsManager.getInstance();
+        boostsManager.boostPlayer(player, power, time);
+        commandSender.sendMessage(ChatColor.GREEN + player.getName() + "'s boost is now: x"
+                + boostsManager.getPlayerBoostMap().get(player).getMultiplier()
+                + " and remains for another: "
+                + (boostsManager.getPlayerBoostMap().get(player).getDuration() - boostsManager.getPlayerBoostMap().get(player).getActiveFor())
+                + "min"
+        );
     }
 }
