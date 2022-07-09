0119850751922d8fbdcc306f05d3b329ed23bb3e
Yazil <yazil@gmx.fr>
2022-07-09 12:31:11 +0200

    Bar teleporting task

--- src/main/java/fr/yazil/boost/BoostPlugin.java
+++ src/main/java/fr/yazil/boost/BoostPlugin.java
@@ -11,6 +11,18 @@ public class BoostPlugin extends JavaPlugin {
     public void onEnable() {
         getServer().getPluginCommand("boost").setExecutor(new BoostExecutor());
         getServer().getScheduler().scheduleSyncRepeatingTask(this, new BoostUpdateLoop(), 200, 200);
+        //Update the virtual wither pos for the bossbar
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                if(Bukkit.getOnlinePlayers().size() == 0)
+                    return;
+
+                for(Player p: Bukkit.getOnlinePlayers()) {
+                    BossBarUtils.getInstance().teleportBar(p);
+                }
+            }
+        }, 20 , 20);
     }
 
 
