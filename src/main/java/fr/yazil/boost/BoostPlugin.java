0cbcfc3d90eb8584ffccc6757657ec808de47a80
Yazil <yazil@gmx.fr>
2022-07-09 14:05:44 +0200

    Register the listener and fix timing of update loop

--- src/main/java/fr/yazil/boost/BoostPlugin.java
+++ src/main/java/fr/yazil/boost/BoostPlugin.java
@@ -2,15 +2,19 @@ package fr.yazil.boost;
 
 import fr.yazil.boost.boosts.BoostUpdateLoop;
 import fr.yazil.boost.commands.BoostExecutor;
-import lombok.Getter;
+import fr.yazil.boost.utils.BossBarUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class BoostPlugin extends JavaPlugin {
+public class BoostPlugin extends JavaPlugin{
 
     @Override
     public void onEnable() {
         getServer().getPluginCommand("boost").setExecutor(new BoostExecutor());
-        getServer().getScheduler().scheduleSyncRepeatingTask(this, new BoostUpdateLoop(), 200, 200);
+        getServer().getScheduler().scheduleSyncRepeatingTask(this, new BoostUpdateLoop(), 1200, 1200);
+        getServer().getPluginManager().registerEvents(new BoostListener(), this);
+
         //Update the virtual wither pos for the bossbar
         getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
             @Override
